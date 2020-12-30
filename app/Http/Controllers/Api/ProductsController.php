6343c6ab7980d93f0c7356942ded5dbce8dac58c
alvis.skeps <alvis.skeps98@gmail.com>
2020-12-30 16:53:26 +0200

    Created CRUD actions for Product

---
+++ app/Http/Controllers/Api/ProductsController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\Routing\ResponseFactory;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class ProductsController extends Controller
+{
+    /**
+     * @return Application|ResponseFactory|Response
+     */
+    public function getAllProducts()
+    {
+        $products = Product::get()->toJson(JSON_PRETTY_PRINT);
+        return response($products, 200);
+    }
+
+    /**
+     * @param int $id
+     * @return Application|ResponseFactory|JsonResponse|Response
+     */
+    public function getProduct(int $id)
+    {
+        if (Product::where('id', $id)->exists()) {
+            $product = Product::where('id', $id)->get()->toJson(JSON_PRETTY_PRINT);
+
+            return response($product, 200);
+        } else {
+            return response()->json([
+                "message" => "product not found"
+            ], 404);
+        }
+    }
+
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function createProduct(Request $request) {
+        $product = new Product;
+        $product->name = $request->name;
+        $product->description = $request->description;
+        $product->price = $request->price;
+        $product->amount = $request->amount;
+        $product->save();
+
+        return response()->json([
+            "message" => "product record created"
+        ], 201);
+    }
+
+    /**
+     * @param Request $request
+     * @param int $id
+     * @return JsonResponse
+     */
+    public function updateProduct(Request $request, int $id) {
+        if (Product::where('id', $id)->exists()) {
+            $product = Product::find($id);
+
+            $product->name = $request->name;
+            $product->description = $request->description;
+            $product->price = $request->price;
+            $product->amount = $request->amount;
+            $product->save();
+
+            return response()->json([
+                "message" => "records updated successfully"
+            ], 200);
+        } else {
+            return response()->json([
+                "message" => "product not found"
+            ], 404);
+        }
+    }
+
+    /**
+     * @param int $id
+     * @return JsonResponse
+     */
+    public function deleteProduct(int $id) {
+        if (Product::where('id', $id)->exists()) {
+            $product = Product::find($id);
+            $product->delete();
+
+            return response()->json([
+                "message" => "records deleted"
+            ], 202);
+        } else {
+            return response()->json([
+                "message" => "product not found"
+            ], 404);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\ProductsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('products', [ProductsController::class, 'getAllProducts']);
+Route::get('products/{id}', [ProductsController::class, 'getProduct']);
+Route::post('products', [ProductsController::class, 'createProduct']);
+Route::put('products/{id}', [ProductsController::class, 'updateProduct']);
+Route::delete('products/{id}', [ProductsController::class, 'deleteProduct']);
